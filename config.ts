d53c53f83c3e6a74da4e88be39536bafc79bbd6f
luowei <glpat-EjySCyNjWiLqAED-YmwM>
2023-12-04 20:37:19 +0800

    临时屏蔽会话删除删选条件

--- config.ts
+++ config.ts
@@ -6,7 +6,7 @@ export const defaultConfig: Config = {
     url: "https://hc.jiandaoyun.com/doc/",
     match: "https://hc.jiandaoyun.com/doc/**",
     inclusions: [".page-title",".markdown-body"],
-    maxPagesToCrawl: 9000,
+    maxPagesToCrawl: 90000,
     waitForSelectorTimeout: 2000,
     outputType: "database",
     outputFileName: "/Users/luowei/Documents/Pycharm/Crawler/output.json",
